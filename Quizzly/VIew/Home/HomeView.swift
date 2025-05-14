f3595da9f5245b26633283e678de0586e3ac054d
Tedd <tddy.ko@kakao.com>
2025-05-15 00:28:58 +0900

    [refactor]
    - 홈뷰 컴포넌트 분리

--- Quizzly/VIew/Home/HomeView.swift
+++ Quizzly/VIew/Home/HomeView.swift
@@ -40,195 +40,222 @@ struct HomeView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(spacing: 24) {
-                Group {
+                
+                backgroundView {
                     VStack(alignment: .leading, spacing: 15) {
-                        // MARK: 프로필 및 인사말
-                        HStack(spacing: 10) {
-                            Circle()
-                                .fill(profile.themeColorHex?.asHexColor ?? .gray)
-                                .frame(width: 45, height: 45)
-                                .overlay {
-                                    Text(profile.name.prefix(1))
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.white)
-                                }
-                                .onTapGesture {
-                                    navigationPath = NavigationPath()
-                                }
-                            
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("\(profile.name)님, 안녕하세요!")
-                                    .font(.subheadline)
-                                    .fontWeight(.semibold)
-                                
-                                Text("학습 계속해보세요")
-                                    .font(.footnote)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.black.opacity(0.5))
-                            }
-                            
-                            Spacer()
-                        }
                         
-                        // MARK: 전체 정답률
-                        HStack {
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text("전체 정답률")
-                                    .font(.footnote)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.black.opacity(0.5))
-                                
-                                // TODO: 실제 퍼센트로 수정
-                                Text("78%")
-                                    .font(.title3)
-                                    .bold()
-                            }
-                            
-                            Spacer()
-                            
-                            ZStack {
-                                Circle()
-                                    .stroke(.gray.opacity(0.3), lineWidth: 10)
-                                    .frame(width: 55, height: 55)
-                                
-                                Circle()
-                                    .rotation(.degrees(-90))
-                                    .trim(from: 0, to: 0.78)
-                                    .stroke(.blue, style: .init(lineWidth: 10, lineCap: .round))
-                                    .frame(width: 55, height: 55)
-                            }
-                            .clipShape(.circle)
-                        }
-                        .padding(13)
-                        .background(.blue.opacity(0.08))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(.blue.opacity(0.05), lineWidth: 1)
+                        profileView(
+                            name: profile.name,
+                            themeColorHex: profile.themeColorHex
                         )
-                        .cornerRadius(8)
                         
-                        // MARK: 추천 퀴즈
-                        Text("추천 퀴즈")
-                            .font(.headline)
+                        totalCorrectRateView()
                         
-                        HStack {
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text("알고리즘 기초 퀴즈")
-                                    .fontWeight(.semibold)
-                                
-                                Text("취약점 보완 추천")
-                                    .font(.footnote)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.black.opacity(0.5))
-                            }
-                            
-                            Spacer()
-                            
-                            Button {
-                                
-                            }label: {
-                                Text("시작")
-                                    .font(.footnote)
-                                    .fontWeight(.semibold)
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 6)
-                                    .background(Color.orange.opacity(0.8))
-                                    .foregroundColor(.white)
-                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                            }
-                        }
-                        .padding(13)
-                        .background(.yellow.opacity(0.09))
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(.orange.opacity(0.1), lineWidth: 1)
-                        )
-                        .cornerRadius(8)
+                        recommendQuizView()
                     }
                 }
-                .padding()
-                .background(.white)
-                .cornerRadius(16)
-                .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 2)
                 
-                Group {
-                    // MARK: 카테고리
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack {
-                            Text("카테고리")
-                                .font(.headline)
-                                .padding(.bottom, 10)
-                            
-                            Spacer()
-                            
-                            Button {
-                                withAnimation {
-                                    showingAllCategories.toggle()
-                                }
-                            } label: {
-                                Text(showingAllCategories ? "간단히 보기" : "모두 보기")
-                                .font(.footnote)
-                            }
-                        }
-                        
-                        let displayedCategories = showingAllCategories ? allCategories : Array(allCategories.prefix(4))
-                        
-                        VStack {
-                            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 12) {
-                                ForEach(displayedCategories) { category in
-                                    Button {
-                                        navigationPath.append(category)
-                                    } label: {
-                                        CategoryCard(
-                                            title: category.title,
-                                            icon: category.icon,
-                                            count: category.count,
-                                            color: category.color
-                                        )
-                                    }
-                                    .buttonStyle(PlainButtonStyle())
-                                }
-                            }
-                        }
-                    }
+                backgroundView {
+                    categoryView(showingAllCategories: $showingAllCategories, allCategories: allCategories)
                 }
-                .padding()
-                .background(.white)
-                .cornerRadius(16)
-                .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 2)
                 
-                Group {
-                    // MARK: 최근 학습
-                    VStack(alignment: .leading, spacing: 13) {
-                        Text("최근 학습")
-                            .font(.headline)
-                            .padding(.bottom, 10)
-                        
-                        ForEach(Array(recentCards.enumerated()), id: \.1.id) { index, card in
-                            VStack {
-                                RecentCard(
-                                    title: card.title,
-                                    percent: card.percent,
-                                    date: card.date,
-                                    isCorrect: card.isCorrect
-                                )
-                            }
-
-                            if index < recentCards.count - 1 {
-                                Divider()
-                            }
-                        }
-                    }
+                
+                backgroundView {
+                    recentQuizView(recentCards: recentCards)
                 }
-                .padding()
-                .background(.white)
-                .cornerRadius(16)
-                .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 2)
             }
             .padding()
         }
         .navigationBarBackButtonHidden(true)
         .background(.gray.opacity(0.1))
+    }
+}
+
+@ViewBuilder
+private func profileView(name: String, themeColorHex: String?) -> some View {
+    // MARK: 프로필 및 인사말
+    HStack(spacing: 10) {
+        Circle()
+            .fill(themeColorHex?.asHexColor ?? .gray)
+            .frame(width: 45, height: 45)
+            .overlay {
+                Text(name.prefix(1))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+            }
+        
+        VStack(alignment: .leading, spacing: 5) {
+            Text("\(name)님, 안녕하세요!")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            
+            Text("학습 계속해보세요")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.black.opacity(0.5))
+        }
+        
+        Spacer()
+    }
+}
+
+@ViewBuilder
+private func totalCorrectRateView() -> some View {
+    // MARK: 전체 정답률
+    HStack {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("전체 정답률")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.black.opacity(0.5))
+            
+            // TODO: 실제 퍼센트로 수정
+            Text("78%")
+                .font(.title3)
+                .bold()
+        }
+        
+        Spacer()
+        
+        ZStack {
+            Circle()
+                .stroke(.gray.opacity(0.3), lineWidth: 10)
+                .frame(width: 55, height: 55)
+            
+            Circle()
+                .rotation(.degrees(-90))
+                .trim(from: 0, to: 0.78)
+                .stroke(.blue, style: .init(lineWidth: 10, lineCap: .round))
+                .frame(width: 55, height: 55)
+        }
+        .clipShape(.circle)
+    }
+    .padding(13)
+    .background(.blue.opacity(0.08))
+    .overlay(
+        RoundedRectangle(cornerRadius: 8)
+            .stroke(.blue.opacity(0.05), lineWidth: 1)
+    )
+    .cornerRadius(8)
+}
+
+@ViewBuilder
+private func recommendQuizView() -> some View {
+    // MARK: 추천 퀴즈
+    Text("추천 퀴즈")
+        .font(.headline)
+    
+    HStack {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("알고리즘 기초 퀴즈")
+                .fontWeight(.semibold)
+            
+            Text("취약점 보완 추천")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.black.opacity(0.5))
+        }
+        
+        Spacer()
+        
+        Button {
+            
+        }label: {
+            Text("시작")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .padding(.horizontal, 10)
+                .padding(.vertical, 6)
+                .background(Color.orange.opacity(0.8))
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
+    }
+    .padding(13)
+    .background(.yellow.opacity(0.09))
+    .overlay(
+        RoundedRectangle(cornerRadius: 8)
+            .stroke(.orange.opacity(0.1), lineWidth: 1)
+    )
+    .cornerRadius(8)
+}
+
+@ViewBuilder
+private func backgroundView(@ViewBuilder content: () -> some View) -> some View {
+    Group {
+        content()
+    }
+    .padding()
+    .background(.white)
+    .cornerRadius(16)
+    .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 2)
+}
+
+@ViewBuilder
+private func categoryView(
+    showingAllCategories: Binding<Bool>,
+    allCategories: [Category],
+) -> some View {
+    // MARK: 카테고리
+    VStack(alignment: .leading, spacing: 10) {
+        HStack {
+            Text("카테고리")
+                .font(.headline)
+                .padding(.bottom, 10)
+            
+            Spacer()
+            
+            Button {
+                withAnimation {
+                    showingAllCategories.wrappedValue.toggle()
+                }
+            } label: {
+                Text(showingAllCategories.wrappedValue ? "간단히 보기" : "모두 보기")
+                .font(.footnote)
+            }
+        }
+        
+        let displayedCategories = showingAllCategories.wrappedValue ? allCategories : Array(allCategories.prefix(4))
+        
+        VStack {
+            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 12) {
+                ForEach(displayedCategories) { category in
+                    CategoryCard(
+                        title: category.title,
+                        icon: category.icon,
+                        count: category.count,
+                        color: category.color
+                    )
+                }
+            }
+        }
+    }
+}
+
+@ViewBuilder
+private func recentQuizView(
+    recentCards: [RecentCard]
+) -> some View {
+    // MARK: 최근 학습
+    VStack(alignment: .leading, spacing: 13) {
+        Text("최근 학습")
+            .font(.headline)
+            .padding(.bottom, 10)
+        
+        ForEach(Array(recentCards.enumerated()), id: \.1.id) { index, card in
+            VStack {
+                RecentCard(
+                    title: card.title,
+                    percent: card.percent,
+                    date: card.date,
+                    isCorrect: card.isCorrect
+                )
+            }
+
+            if index < recentCards.count - 1 {
+                Divider()
+            }
+        }
     }
 }
 
