8a3caa3ea9c201cfbeeca791de0bd3db7f39cf94
Minji Kang <aldalddl2007@gmail.com>
2025-05-14 11:40:28 +0900

    feat: 대시보드 화면 UI 구현

---
+++ Quizzly/VIew/Dashboard/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  Quizzly
+//
+//  Created by 강민지 on 5/14/25.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
