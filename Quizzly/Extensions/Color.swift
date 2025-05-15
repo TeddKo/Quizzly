7bf845ba4862c40d28d285c7532db0ecba4598a7
Minji Kang <aldalddl2007@gmail.com>
2025-05-16 04:05:02 +0900

    feat: 커스텀 컬러 추가

--- Quizzly/Extensions/Color.swift
+++ Quizzly/Extensions/Color.swift
@@ -53,4 +53,54 @@ extension Color {
 
         return hexString
     }
+    
+    static var dynamicBackground: Color {
+        Color(UIColor.systemBackground)
+    }
+    
+    static var adaptiveGrayOverlay: Color {
+        Color(uiColor: UIColor { trait in
+            switch trait.userInterfaceStyle {
+            case .dark:
+                return UIColor(white: 1.0, alpha: 0.1) // 밝은 회색 느낌
+            default:
+                return UIColor.black.withAlphaComponent(0.05)
+            }
+        })
+    }
+    
+    static var adaptiveCyan: Color {
+        Color(uiColor: UIColor { traitCollection in
+            traitCollection.userInterfaceStyle == .dark
+                ? UIColor(red: 0.4, green: 1.0, blue: 1.0, alpha: 1.0)  // 다크모드용
+                : UIColor.systemTeal  // 라이트모드용
+        })
+    }
+    
+    static var adaptiveGreen: Color {
+        Color(uiColor: UIColor { trait in
+            trait.userInterfaceStyle == .dark
+                ? UIColor(red: 0.6, green: 1.0, blue: 0.6, alpha: 1.0)
+                : UIColor.systemGreen
+        })
+    }
+
+    static var adaptiveRed: Color {
+        Color(uiColor: UIColor { trait in
+            trait.userInterfaceStyle == .dark
+                ? UIColor(red: 1.0, green: 0.5, blue: 0.5, alpha: 1.0)
+                : UIColor.systemRed
+        })
+    }
+    
+    static var adaptiveBlue: Color {
+        Color(uiColor: UIColor { trait in
+            switch trait.userInterfaceStyle {
+            case .dark:
+                return UIColor(red: 100/255, green: 180/255, blue: 255/255, alpha: 1.0) // 밝은 하늘색
+            default:
+                return .systemBlue
+            }
+        })
+    }
 }
